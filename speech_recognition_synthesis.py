8e5548de8b3e74f6f15c55a0d7c7b69077511301
bosnj@DESKTOP-8SJ8M8T <mb52661@fer.hr>
2023-04-01 23:45:37 +0200

    Add synthesis and recognition in one script

---
+++ speech_recognition_synthesis.py
@@ -0,0 +1,56 @@
+import os
+import azure.cognitiveservices.speech as speechsdk
+
+# This example requires environment variables named "SPEECH_KEY" and "SPEECH_REGION"
+speech_config = speechsdk.SpeechConfig(subscription=os.environ.get('SPEECH_KEY'), region=os.environ.get('SPEECH_REGION'))
+audio_config = speechsdk.audio.AudioOutputConfig(use_default_speaker=True)
+
+# The language of the voice that speaks.
+speech_config.speech_synthesis_voice_name='hr-HR-SreckoNeural'
+speech_config.speech_recognition_language="hr-HR"
+
+#tekst-govor varijable
+audio_config = speechsdk.audio.AudioConfig(use_default_microphone=True)
+speech_recognizer = speechsdk.SpeechRecognizer(speech_config=speech_config, audio_config=audio_config)
+speech_recognition_result = speech_recognizer.recognize_once_async().get()
+
+#govor-tekst varijabl
+speech_synthesizer = speechsdk.SpeechSynthesizer(speech_config=speech_config, audio_config=audio_config)
+
+print("Speak into your microphone.")
+
+#GOVOR U TEKST
+
+def recognize_from_microphone():
+    # This example requires environment variables named "SPEECH_KEY" and "SPEECH_REGION"
+
+    if speech_recognition_result.reason == speechsdk.ResultReason.RecognizedSpeech:
+        print("Recognized: {}".format(speech_recognition_result.text))  #vraća prepoznati govor u tekstu
+    elif speech_recognition_result.reason == speechsdk.ResultReason.NoMatch:
+        print("No speech could be recognized: {}".format(speech_recognition_result.no_match_details))
+    elif speech_recognition_result.reason == speechsdk.ResultReason.Canceled:
+        cancellation_details = speech_recognition_result.cancellation_details
+        print("Speech Recognition canceled: {}".format(cancellation_details.reason))
+        if cancellation_details.reason == speechsdk.CancellationReason.Error:
+            print("Error details: {}".format(cancellation_details.error_details))
+            print("Did you set the speech resource key and region values?")
+
+recognize_from_microphone()
+
+#TEKST U GOVOR
+
+# Get text from the console and synthesize to the default speaker.
+print("You said >")
+text = speech_recognition_result.text
+
+speech_synthesis_result = speech_synthesizer.speak_text_async(text).get()
+
+if speech_synthesis_result.reason == speechsdk.ResultReason.SynthesizingAudioCompleted:
+    print("Speech synthesized for text [{}]".format(text))
+elif speech_synthesis_result.reason == speechsdk.ResultReason.Canceled:
+    cancellation_details = speech_synthesis_result.cancellation_details
+    print("Speech synthesis canceled: {}".format(cancellation_details.reason))
+    if cancellation_details.reason == speechsdk.CancellationReason.Error:
+        if cancellation_details.error_details:
+            print("Error details: {}".format(cancellation_details.error_details))
+            print("Did you set the speech resource key and region values?")
